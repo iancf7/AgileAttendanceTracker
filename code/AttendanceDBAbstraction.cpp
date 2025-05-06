9b20de8e7a31e47fb41cc5d6d1a4455e078fa369
matty-michel <mmichel@carthage.edu>
2025-05-06 14:33:00 -0500

    finished create table functions in attendanceDB

--- code/AttendanceDBAbstraction.cpp
+++ code/AttendanceDBAbstraction.cpp
@@ -9,19 +9,48 @@ AttendanceDBAbstraction::AttendanceDBAbstraction(string pathToFile)
 {
     //create the attendance tables
     createStudentsTable();
-    createClassesTable();
-    createAttendanceRecordTable();
+    createCoursesTable();
+    createAttendanceRecordsTable();
 }
 //--
-void AttendanceDBAbstraction::createStudentsTable(){
+void AttendanceDBAbstraction::createStudentsTable() {
+    string sql = "CREATE TABLE IF NOT EXISTS Students (id INTEGER PRIMARY KEY NOT NULL, firstName TEXT, lastName TEXT);";
+     
+    //execute the query to create the table
+    if (!executeQueryNoResultsBack(sql))
+    {
+        cout << "Error creating Items table" << endl;
+    }
+}
+//--
+void AttendanceDBAbstraction::createCoursesTable() {
+    string sql = "CREATE TABLE IF NOT EXISTS Courses (id INTEGER PRIMARY KEY NOT NULL, name TEXT, semester TEXT, year INTEGER, meetingSchedule TEXT, startTime TEXT, endTime TEXT, instructor TEXT);";
+     
+    //execute the query to create the table
+    if (!executeQueryNoResultsBack(sql))
+    {
+        cout << "Error creating Items table" << endl;
+    }
+}
+//--
+void AttendanceDBAbstraction::createAttendanceRecordsTable() {
+    string sql = "CREATE TABLE IF NOT EXISTS AttendanceRecords (id INTEGER PRIMARY KEY NOT NULL, meetingDate TEXT, meetingTime TEXT, attendanceStatus TEXT);";
+     
+    //execute the query to create the table
+    if (!executeQueryNoResultsBack(sql))
+    {
+        cout << "Error creating Items table" << endl;
+    }
+}
+//--
+void AttendanceDBAbstraction::insertStudent(int studentID, string first, string last) {
     
 }
 //--
-void AttendanceDBAbstraction::createClassesTable(){
+void AttendanceDBAbstraction::insertCourse(int courseID, string name, string semester, int year, string meetingSchedule, string startTime, string endTime, string instructor) {
     
 }
 //--
-void AttendanceDBAbstraction::createAttendanceRecordTable(){
+void AttendanceDBAbstraction::insertAttendanceRecord(int attendanceID, string meetingDate, string meetingTime, string attendanceStatus) {
     
 }
-//--
--- code/AttendanceDBAbstraction.h
+++ code/AttendanceDBAbstraction.h
@@ -8,11 +8,11 @@ public:
     AttendanceDBAbstraction(string pathToFile);
     
     void insertStudent(int studentID, string first, string last);
-    void insertClass(int courseID, string name, string semester, int year, string meetingSchedule, string startTime, string endTime, string instructor);
+    void insertCourse(int courseID, string name, string semester, int year, string meetingSchedule, string startTime, string endTime, string instructor);
     void insertAttendanceRecord(int attendanceID, string meetingDate, string meetingTime, string attendanceStatus);
     
 private:
     void createStudentsTable();
-    void createClassesTable();
-    void createAttendanceRecordTable();
+    void createCoursesTable();
+    void createAttendanceRecordsTable();
 };

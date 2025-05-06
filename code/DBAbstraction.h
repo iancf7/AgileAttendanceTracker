8c4f5bbe247cfe03c5af563055f2ebebc8a67af7
matty-michel <mmichel@carthage.edu>
2025-05-06 13:06:13 -0500

    created DBAbstraction class

---
+++ code/DBAbstraction.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+#include "sqlite3.h"
+ 
+using namespace std;
+
+class DBAbstraction {
+public:
+    DBAbstraction(string pathToFile);
+    ~DBAbstraction();
+protected:
+    sqlite3* db;
+    
+    //private helpers to execute queries
+    bool executeQueryNoResultsBack(sqlite3_stmt* myStatement);
+    bool executeQueryNoResultsBack(string sql);
+    bool prepareQueryWithResults(string sql, sqlite3_stmt*& myStatement);
+};

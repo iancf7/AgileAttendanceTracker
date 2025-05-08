ac69ef8cfd3f1ececa28af73613ecd071c8e3805
iancf7 <iflaskamp@carthage.edu>
2025-05-08 14:00:58 -0500

    addClass() added.

--- code/main.cpp
+++ code/main.cpp
@@ -19,7 +19,7 @@ int main()
         cout<<"Please enter an option:"<<endl;
         cout<<"1) Add a class"<<endl;
         cout<<"2) Add a student to a class"<<endl;
-        cout<<"3) View all students in a class"<<endl;
+        cout<<"3) View the attendance for a class"<<endl;
         cout<<"4) Take attendance for a class"<<endl;
         cout<<"5) Quit"<<endl;
         cout<<"======================================"<<endl;
@@ -50,6 +50,37 @@ int main()
 
 void addClass() {
 
+    string courseName;
+    string semesterOffered;
+    int yearOffered;
+    string meetingSchedule;
+    string startTime;
+    string endTime;
+    string instructor;
+
+    cout<<"Enter in the name of the course: "<<endl;
+    cin >> courseName;
+
+    cout<<"Enter in the semester the course is offered (ex: Fall)"<<endl;
+    cin >> semesterOffered;
+
+    cout<<"Enter in the year the course is offered: "<<endl;
+    cin >> yearOffered;
+
+    cout<<"Enter in the meeting schedule: (ex: MWF, TTR)"<<endl;
+    cin >> meetingSchedule;
+
+    cout<<"Enter in the start time of the course: (format: XX:XX AM/PM)"<<endl;
+    cin >> startTime;
+
+    cout<<"Enter in the end time of the course: (format: XX:XX AM/PM)"<<endl;
+    cin >> endTime;
+
+    cout<<"Enter in the name of the course instructor: "<<endl;
+    cin >> endTime;
+
+    //Add to database
+
 
 }
 
@@ -58,10 +89,10 @@ void addStudent() {
     string firstName;
     string lastName;
 
-    cout<<"Enter in the student's first name: ";
+    cout<<"Enter in the student's first name: "<<endl;
     cin >> firstName;
 
-    cout<<"Enter in the student's last name: ";
+    cout<<"Enter in the student's last name: "<<endl;
     cin >> lastName;
     
     //Add to database
@@ -69,6 +100,8 @@ void addStudent() {
 }
 
 void viewStudents() {
+
+    
 
 
 }
